4084c85428edd06c2401705035178f3b97bd2489
vlad961 <vlad96@hotmail.de>
2022-04-15 09:16:59 +0200

    refactoring and adding current JOURNEYS Mockup structure

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { JourneyRoutingModule } from './journey-content/journey-routing.module';
+import { JourneyContentModule } from './journey-content/journey-content.module';
 
 import { AppComponent } from './app.component';
 
+import { PageNotFoundComponent } from './journey-content/page-not-found/page-not-found.component';
+import { MessagesComponent } from './messages/messages.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    MessagesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    JourneyContentModule,
+    JourneyRoutingModule,
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
