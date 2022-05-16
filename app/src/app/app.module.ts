063a71cff81749781154601c28c74e7f2e0f0a01
ykernerm <yana.kernerman@capgemini.com>
2022-05-16 12:31:04 +0200

    final ngrx and angular app version with visualisation of store data

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -16,10 +16,19 @@ import { JourneyContentModule } from './journey-content/journey-content.module';
 import { HttpClientModule } from '@angular/common/http';
 import { PageNotFoundComponent } from './journey-content/page-not-found/page-not-found.component';
 import { MessagesComponent } from './messages/messages.component';
-import { JourneyService } from './journey-content/journey.service';
-//NGRX imports
+import { JourneyService } from './state/journeys/journey.service';
+import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
-import { counterReducer } from './store/my-counter/counter.reducer';
+import { JourneyDetailComponent } from './journey-content/journey-detail/journey-detail.component';
+import { JourneyListComponent } from './journey-content/journey-list/journey-list.component';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { JourneyEffect } from './state/journeys/journey.effect';
+import appReducer from './state/journeys//journey.reducer';
+import { AppState, DataState } from './state/app.state';
+import {getAppState} from './state/journeys/journey.selector'
+
 
 @NgModule({
   declarations: [
@@ -28,10 +37,11 @@ import { counterReducer } from './store/my-counter/counter.reducer';
     MessagesComponent,
     HomeComponent,
     DataComponent,
+    JourneyDetailComponent,
+    JourneyListComponent,
   ],
   imports: [
     BrowserModule,
-    JourneyContentModule,
     JourneyRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
@@ -42,7 +52,15 @@ import { counterReducer } from './store/my-counter/counter.reducer';
     MatToolbarModule,
     MatSidenavModule,
     AppRoutingModule,
-    StoreModule.forRoot({ count: counterReducer })
+    CommonModule,
+    JourneyRoutingModule,
+    StoreModule.forRoot({ journeyData: appReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+    EffectsModule.forRoot([JourneyEffect])
+
   ],
   providers: [JourneyService],
   bootstrap: [AppComponent]
