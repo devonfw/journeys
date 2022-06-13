c226d0c649cc8bb023a2d14f45c59e26f082d6b9
moritzLanger <Moritz.langer2@web.de>
2022-06-13 09:39:28 +0200

    work update

--- app/src/app/journey-content/step-detail/step-detail.component.ts
+++ app/src/app/journey-content/step-detail/step-detail.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { AppState, StepData } from '../../state/app.state';
+import { AppState, JourneyData, StepData } from '../../state/app.state';
 import { Observable } from 'rxjs';
 import { loadStep } from '../../state/steps/step.actions';
 import { getStepDataState, findIndexStepExistence, getJourneySection } from '../../state/steps/step.selector';
 import {takeUntil, take} from 'rxjs/operators';
+import { getDataState } from '../../state/journeys/journey.selector';
 
 @Component({
   selector: 'app-step-detail',
@@ -19,30 +20,32 @@ export class StepDetailComponent implements OnInit {
   step$: Observable<StepData>;
   index$: Observable<any>;
   journeySection$: Observable<any>;
+  journey$: Observable<JourneyData>;
 
   constructor(private store: Store<AppState>, private router: Router, private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
+
     this.route.paramMap.subscribe((params: ParamMap) => {
       let id = params.get('stepId');
       this.index$ = this.store.select(findIndexStepExistence({ step_id: id }))
-      
+      this.journey$ = this.store.select(getDataState)
       this.index$.pipe(take(1)).subscribe(data => {
         if (data == -1) {
           this.store.dispatch(loadStep({ stepId: id }));
-         
         }
-        this.journeySection$ = this.store.select(getJourneySection)
-        this.journeySection$.pipe(take(1)).subscribe(sectionData => {
-          let stepIdData = sectionData.sections.filter(x => !!x && x.id == id)[0]
-          if (stepIdData.sections.length > 0) {
-            console.log(stepIdData.sections)
-            this.getSubSectionIds(stepIdData.sections)
-          }
-          })
-      });
-         
+
+      })
+      this.journeySection$ = this.store.select(getJourneySection)
+      this.journeySection$.pipe(take(1)).subscribe(sectionData => {
+        let stepIdData = sectionData.sections.filter(x => !!x && x.id == id)[0]
+        if (stepIdData.sections.length > 0) {
+          this.getSubSectionIds(stepIdData.sections)
+        }
+      })
+
+      this.index$ = this.store.select(findIndexStepExistence({ step_id: id }))
     });
     this.step$ = this.store.select(getStepDataState)
   }
@@ -54,7 +57,7 @@ export class StepDetailComponent implements OnInit {
           console.log(indexData)
           if (indexData == -1) {
               console.log("loadStep")
-              this.store.dispatch(loadStep({ stepId: data[i].id }));
+            this.store.dispatch(loadStep({ stepId: data[i].id }));
           }})
         if (data[i].sections.length > 0) {
           console.log(data[i].sections)
