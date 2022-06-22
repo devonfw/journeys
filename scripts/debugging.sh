7aabb0487664d614d0ca828ffb3642cf7b96c5ac
moritzLanger <44358321+moritzLanger@users.noreply.github.com>
2022-06-22 11:55:03 +0200

    removed test cmds

--- scripts/debugging.sh
+++ scripts/debugging.sh
@@ -1,19 +1,8 @@
 echo "Start Debugging Script"
 ls -l
-ls dist -l
-ls dist/devon4ng-mat-layout/ -l
-echo "cp ohne . "
-cp -avr /dist/devon4ng-mat-layout/ target/generated-docs/website/pages/journeys/
 
-ls target/generated-docs/website/pages/journeys/
-echo "cp mit *"
-cp -avr /dist/devon4ng-mat-layout/* target/generated-docs/website/pages/journeys/
-ls target/generated-docs/website/pages/journeys/
 
-echo "cp ohne . Punkt mit ../.."
-cp -avr /dist/devon4ng-mat-layout/ ../../target/generated-docs/website/pages/journeys/
+echo "ls target generated docs"
 ls ../../target/generated-docs/website/pages/journeys/
-echo "cp mit * Punkt mit ../.."
-cp -avr /dist/devon4ng-mat-layout/* ../../target/generated-docs/website/pages/journeys/
 
 echo "End Debugging Script"
