ff5b1810b3cfe2e271fb9b3bc6c74f30ec4c4c7d
vlad961 <vlad96@hotmail.de>
2022-06-22 10:36:32 +0200

    added debugging script for build-and-deploy.yml

---
+++ scripts/debugging.sh
@@ -0,0 +1,6 @@
+echo "End Debugging Script"
+ls -l
+ls dist -l
+ls dist/devon4ng-mat-layout/ -l
+cp -avr /dist/devon4ng-mat-layout/. target/generated-docs/website/pages/journeys/
+echo "End Debugging Script"
